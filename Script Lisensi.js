17c3e56f3cad9c1bee9120607f2417f24033eb49
daniafderi <56910041+daniafderi@users.noreply.github.com>
2021-11-10 22:38:01 +0700

    Update Script Lisensi.js

--- Script Lisensi.js
+++ Script Lisensi.js
@@ -1,1 +1,1 @@
-var a="kumapoi.blogspot.com",b="https://pemudanolep.blogspot.com",c="https://pemudanolep.blogspot.com";window.location.href.includes(a)||window.location.href.includes(b)||window.location.href.includes(c)||(window.location.href="https://pemudanolep.blogspot.com");
+var a="nodomain",b="https://pemudanolep.blogspot.com",c="https://pemudanolep.blogspot.com";window.location.href.includes(a)||window.location.href.includes(b)||window.location.href.includes(c)||(window.location.href="https://pemudanolep.blogspot.com");
